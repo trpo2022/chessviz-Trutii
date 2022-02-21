73215f4e9997c1f94d3660ddc93b4fc627348e8b
Trutii <Truti-matruti@mail.ru>
2022-02-21 12:24:52 +0700

    create function printChess

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#define chess_size 8
+void printChess()
+{
+	char Desk[8][8]=
+	{"rnbqkbnr",
+	"pppppppp",
+	"        ",
+	"        ",
+	"        ",
+	"        ",
+	"PPPPPPPP",
+	"RNBQKBNR"};
+	for (int i=0;i<chess_size;i++)
+		for (int j=0; j<chess_size;j++)
+			printf("%c",Desk[i][j]);
+}
+
 int main()
 {
-    printf("Hello, World!");
+    printChess();
     return 0;
 }
