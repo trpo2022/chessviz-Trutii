ad915c220439c36256a1d570d086150190a5a18e
Trutii <Truti-matruti@mail.ru>
2022-02-21 12:39:12 +0700

    create desk

--- main.c
+++ main.c
@@ -14,9 +14,14 @@ void printChess()
 	for (int i=0;i<chess_size;i++)
 	{
 		printf("\n");
+		printf("%i |", i);
 		for (int j=0; j<chess_size;j++)
+		{
 			printf("%c|",Desk[i][j]);
+		}
 	}
+	printf("\n");
+    	printf("  |a|b|c|d|e|f|g|h|\n");
 }
 
 int main()
